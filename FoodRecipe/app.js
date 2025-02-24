c89fc35bcb282167fde00858ba5265da7d1f40e8
Alka Tripathi <tripathialka382@gmail.com>
2025-02-24 22:31:44 +0530

    playing with APIs

--- FoodRecipe/app.js
+++ FoodRecipe/app.js
@@ -1,21 +1,21 @@
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=pasta")
-.then(response => response.json())
-.then(data => {
-    if (!data.meals) {
-        console.log("No meals found");
-        return;
-    }
+// fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=pasta")
+// .then(response => response.json())
+// .then(data => {
+//     if (!data.meals) {
+//         console.log("No meals found");
+//         return;
+//     }
     
-    const meal = data.meals[0]; // Get the first meal result
-    const mealName = meal.strMeal;
-    const category = meal.strCategory;
-    const instructions = meal.strInstructions;
+//     const meal = data.meals[0]; // Get the first meal result
+//     const mealName = meal.strMeal;
+//     const category = meal.strCategory;
+//     const instructions = meal.strInstructions;
 
-    console.log("Meal name:", mealName);
-    console.log("Category:", category);
-    console.log("Instructions:", instructions);
-})
-.catch(error => console.error("Error:", error));
+//     console.log("Meal name:", mealName);
+//     console.log("Category:", category);
+//     console.log("Instructions:", instructions);
+// })
+// .catch(error => console.error("Error:", error));
 
     
 // }
